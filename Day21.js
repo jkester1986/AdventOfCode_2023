72ad2688cc32ba7d90965116d0daa5713342a91d
Julia Kester <jkester@acvauctions.com>
2023-12-21 07:54:57 -0500

    Some Day 21 P2 prep

--- Day21.js
+++ Day21.js
@@ -4,6 +4,8 @@ fs.readFile('Day21.txt', 'utf8', function (err, data) {
     return console.log(err);
   }
   let lines = data.split('\n');
+  let lastYInd = lines.length - 1;
+  let lastXInd = lines[0].length - 1;
   let gardenPlots = {};
 
   let currLoc = { x: 0, y: 0 };
@@ -17,25 +19,39 @@ fs.readFile('Day21.txt', 'utf8', function (err, data) {
 
   let steps = 0;
   let oldLocations = new Set();
-  oldLocations.add(`${currLoc.x},${currLoc.y}`);
+  oldLocations.add(`0,0,${currLoc.x},${currLoc.y}`);
   let newLocations = new Set();
   let validPlots = ["S", "."];
-  while(steps < 64) {
+  while (steps < 5000) {
     steps++; // take a step
+    console.log("step:", steps);
     oldLocations.forEach(location => {
-      let [x, y] = location.split(",").map(Number);
+      let [gardenX, gardenY, x, y] = location.split(",").map(Number);
+      // console.log({ gardenX, gardenY, x, y })
       // check up, down, left, right
-      if (validPlots.includes(gardenPlots[`${x},${y - 1}`]) && !newLocations.has(`${x},${y - 1}`)) {
-        newLocations.add(`${x},${y - 1}`);
+      if (validPlots.includes(gardenPlots[`${x},${y - 1}`])) {
+        newLocations.add(`${gardenX},${gardenY},${x},${y - 1}`);
       }
-      if (validPlots.includes(gardenPlots[`${x},${y + 1}`]) && !newLocations.has(`${x},${y + 1}`)) {
-        newLocations.add(`${x},${y + 1}`);
+      else if (y === 0 && validPlots.includes(gardenPlots[`${x},${lastYInd}`])){
+        newLocations.add(`${gardenX},${gardenY - 1},${x},${lastYInd}`);
       }
-      if (validPlots.includes(gardenPlots[`${x - 1},${y}`]) && !newLocations.has(`${x - 1},${y}`)) {
-        newLocations.add(`${x - 1},${y}`);
+      if (validPlots.includes(gardenPlots[`${x},${y + 1}`])) {
+        newLocations.add(`${gardenX},${gardenY},${x},${y + 1}`);
       }
-      if (validPlots.includes(gardenPlots[`${x + 1},${y}`]) && !newLocations.has(`${x + 1},${y}`)) {
-        newLocations.add(`${x + 1},${y}`);
+      else if (y === lastYInd && validPlots.includes(gardenPlots[`${x},0`])) {
+        newLocations.add(`${gardenX},${gardenY + 1},${x},0`);
+      }
+      if (validPlots.includes(gardenPlots[`${x - 1},${y}`])) {
+        newLocations.add(`${gardenX},${gardenY},${x - 1},${y}`);
+      }
+      else if(x === 0 && validPlots.includes(gardenPlots[`${lastXInd},${y}`])) {
+        newLocations.add(`${gardenX - 1},${gardenY},${lastXInd},${y}`);
+      }
+      if (validPlots.includes(gardenPlots[`${x + 1},${y}`])) {
+        newLocations.add(`${gardenX},${gardenY},${x + 1},${y}`);
+      }
+      else if(x === lastXInd && validPlots.includes(gardenPlots[`0,${y}`])) {
+        newLocations.add(`${gardenX + 1},${gardenY},0,${y}`);
       }
     });
 
@@ -44,19 +60,21 @@ fs.readFile('Day21.txt', 'utf8', function (err, data) {
 
 
 
-    lines.forEach((line, y) => {
-      let plots = line.split("");
-      let lineText = "";
-      plots.forEach((plot, x) => {
-        if (oldLocations.has(`${x},${y}`)) {
-          lineText += "O";
-        }
-        else {
-          lineText += plot;
-        }
-      });
-      // console.log(lineText);
-    })
+    // lines.forEach((line, y) => {
+    //   let plots = line.split("");
+    //   let lineText = "";
+    //   plots.forEach((plot, x) => {
+    //     if (oldLocations.has(`0,0,${x},${y}`)) {
+    //       lineText += "O";
+    //     }
+    //     else {
+    //       lineText += plot;
+    //     }
+    //   });
+    //   console.log(lineText);
+    // })
+
+    // console.log("\n\n")
   }
   console.log("reachable:", oldLocations.size)
 
